2047060e47b37e56961ba3898ca6443eece6670d
pjsm <pjsm.lesi@gmail.com>
2025-03-25 20:12:37 +0000

    Updated: Program.cs

--- Module3/AlgorithmStructures/Problem1/Program.cs
+++ Module3/AlgorithmStructures/Problem1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Problem1 {
+    static void IsDiscountEligible(float totalCost) {
+        if (totalCost >= 100) {
+            Console.WriteLine("10% discount applied.");
+            Console.WriteLine("Updated cost: " + (totalCost * 0.9));
+        }
+        else {
+            Console.WriteLine("No discount.");
+            Console.WriteLine("Cost: " + totalCost);
+        }
+    }
+
+    static void Main(string[] args) {
+        float totalCost;
+        Console.Write("Total Cost: ");
+        totalCost = Convert.ToSingle(Console.ReadLine());
+        IsDiscountEligible(totalCost);
+    }
+}
