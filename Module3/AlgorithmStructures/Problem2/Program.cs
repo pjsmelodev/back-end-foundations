394a68f5e7b6db7766ec8b61083755c5b4dc28e3
pjsm <pjsm.lesi@gmail.com>
2025-03-25 20:21:57 +0000

    Created: Problem2

---
+++ Module3/AlgorithmStructures/Problem2/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program2 {
+    static void CheckGenre(string genre) {
+        switch (genre) {
+            case "Fiction":
+                Console.WriteLine("Category: Fiction.");
+                break;
+            case "Non-Fiction":
+                Console.WriteLine("Category: Non-Fiction.");
+                break;
+            case "Science Fiction":
+                Console.WriteLine("Category: Science Fiction.");
+                break;
+            default:
+                Console.WriteLine("Category: Unknown.");
+                break;
+        }
+    }
+
+    static void Main(string[] args) {
+        Console.Write("Enter book genre: ");
+        string genre = Console.ReadLine();
+
+        CheckGenre(genre);
+    }
+}
