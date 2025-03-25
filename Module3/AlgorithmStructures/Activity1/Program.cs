fa791e1ccb92edc9769571282c27aa965a971dd8
pjsm <pjsm.lesi@gmail.com>
2025-03-25 18:53:08 +0000

    Updated: Program.cs

--- Module3/AlgorithmStructures/Activity1/Program.cs
+++ Module3/AlgorithmStructures/Activity1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Activity1 {
+    // Example 1:
+    static void VotingEligibility(int age) {
+        if (age >= 18) {
+            Console.WriteLine("You are eligible to vote.");
+        }
+        else {
+            Console.WriteLine("You are not eligible to vote yet.");
+        }
+    }
+
+    static void Main(string[] args) {
+        int age;
+
+        Console.Write("Enter age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        VotingEligibility(age);
+    }
+}
