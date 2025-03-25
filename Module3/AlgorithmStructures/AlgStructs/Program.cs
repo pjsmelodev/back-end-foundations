689e8772bbb65a246997f1f268d4d6d86a44c317
pjsm <pjsm.lesi@gmail.com>
2025-03-25 18:24:29 +0000

    Updated: Program.cs

--- Module3/AlgorithmStructures/AlgStructs/Program.cs
+++ Module3/AlgorithmStructures/AlgStructs/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+class Program {
+    public static void Main(string[] args) {
+        int age;
+
+        Console.Write("How old are you? ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        if (age >= 18) {
+            Console.WriteLine("You are eligible to vote.");
+        }
+        else {
+            Console.WriteLine("You are not eligible to vote.");
+        }
+    }
+}
