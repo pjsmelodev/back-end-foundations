3faa2de36dcb035ccb5abcee275d42415a41a471
pjsm <pjsm.lesi@gmail.com>
2025-03-25 20:26:20 +0000

    Updated: Problem3

--- Module3/Problem3/Program.cs
+++ Module3/Problem3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Problem3 {
+    static void Parity(int number) {
+        if (number % 2 == 0) {
+            Console.WriteLine("Even number.");
+        }
+        else {
+            Console.WriteLine("Odd number");
+        }
+    }
+
+    static void Main(string[] args) {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Parity(number);
+    }
+}
