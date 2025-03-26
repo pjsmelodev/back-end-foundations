611431b63ef2fa1b02690ad1d2457e87d8a11a10
pjsm <pjsm.lesi@gmail.com>
2025-03-26 18:29:26 +0000

    Created: TryParse

---
+++ Module3/DataTypes/TryParse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class TryParse {
+    static void Main(string[] args) {
+        string myVariable = "123";
+
+        if (int.TryParse(myVariable, out int number)) {
+            Console.WriteLine($"Conversion successful: {number}");
+        }
+        else {
+            Console.WriteLine($"Conversion failed");
+        }
+    }
+}
