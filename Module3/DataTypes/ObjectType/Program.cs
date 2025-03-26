80aa906bec251bd06053cbe2f92df9a4be44d040
pjsm <pjsm.lesi@gmail.com>
2025-03-26 18:21:04 +0000

    Updated: notes, Created: ObjectType

---
+++ Module3/DataTypes/ObjectType/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class ObjectType {
+    static void Main(string[] args) {
+        object myVariable = 123;
+
+        if (myVariable.GetType() == typeof(int)) {
+            Console.WriteLine($"The variable {myVariable} is an integer.");
+        }
+        else {
+            Console.WriteLine($"The variable {myVariable} is not an integer.");
+        }
+    }
+}
