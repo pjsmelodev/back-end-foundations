8e9c6fd7fe1180bc671b5469f77a03db069ae2f1
pjsm <pjsm.lesi@gmail.com>
2025-03-26 21:57:23 +0000

    Created: Activity1

---
+++ Module3/VariableDeclaration/Activity1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Activity1 {
+    static void Main(string[] args) {
+        // Example 1
+        int numberOfStudents = 25;
+
+        // Example 2
+        string courseName = "Introduction to Programming";
+
+        // Problem 1
+        float productPrice = 49.99F;
+
+        // Problem 2
+        bool isAdmin = true;
+
+        Console.WriteLine($"Example 1: The number of students is: {numberOfStudents}.");
+        Console.WriteLine($"Example 2: The course's name is: '{courseName}'.");
+        Console.WriteLine($"Problem 1: The product's price is: {productPrice}.");
+        Console.WriteLine($"Problem 2: Is he/she admin? {isAdmin}!");
+    }
+}
