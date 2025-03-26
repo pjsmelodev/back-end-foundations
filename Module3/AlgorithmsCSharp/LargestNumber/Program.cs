f495aaad12d866babbfbdfc9db7e6582ea06bb79
pjsm <pjsm.lesi@gmail.com>
2025-03-26 22:17:23 +0000

    Created: LargestNumber

---
+++ Module3/AlgorithmsCSharp/LargestNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class LargestNumber {
+    static void pickLargest(int[] numbers) {
+        int largestNumber = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (largestNumber < numbers[i])
+            {
+                largestNumber = numbers[i];
+            }
+        }
+
+        Console.WriteLine("The largest number in the list is: " + largestNumber);
+    }
+    static void Main(string[] args) {
+        int[] numbers = {7, 2, 8, 3};
+
+        pickLargest(numbers);
+    }
+}
