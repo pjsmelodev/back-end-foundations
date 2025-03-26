701e8d1bbdd1a3efa2fc292c440b3f279a11c50c
pjsm <pjsm.lesi@gmail.com>
2025-03-26 22:24:28 +0000

    Created: TranslateAlg, Updated: notes

---
+++ Module3/AlgorithmsCSharp/TranslateAlg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class TranslateAlg {
+    static void Main(string[] args) {
+        int number1 = 5;
+        int number2 = 10;
+        int number3 = 15;
+
+        int sum = number1 + number2 + number3;
+
+        Console.WriteLine($"The sum of: {number1}, {number2}, {number3} is: {sum}");
+    }
+}
