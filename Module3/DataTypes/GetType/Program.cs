184e0c2799707d2e297f4f6235ca5f35aa913267
pjsm <pjsm.lesi@gmail.com>
2025-03-26 18:13:01 +0000

    Created: GetType Updated: notes

---
+++ Module3/DataTypes/GetType/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class GetType {
+    static void Main(string[] args) {
+        string myVariable = "This fruit is an apple";
+        Type dataType = myVariable.GetType();
+        
+        Console.WriteLine(dataType);
+    }
+}
