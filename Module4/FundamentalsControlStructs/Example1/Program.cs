06d7022b202b21ce4ba8b1ac521c2d2dee6939e8
pjsm <pjsm.lesi@gmail.com>
2025-03-27 20:36:52 +0000

    Created: Example1

---
+++ Module4/FundamentalsControlStructs/Example1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Example1 {
+    static void Main(string[] args) {
+        int age;
+
+        Console.Write("Enter your age: ");
+        age = int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Access granted.");
+        }
+        else {
+            Console.WriteLine("Access denied.");
+        }
+    }
+}
