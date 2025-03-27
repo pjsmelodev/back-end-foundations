f1fb2a109be1cb5b6770608d73caed89f9ed90af
pjsm <pjsm.lesi@gmail.com>
2025-03-27 20:47:11 +0000

    Created: Example2

---
+++ Module4/FundamentalsControlStructs/Example2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Example2 {
+    static void Main(string[] args) {
+        Console.WriteLine("Select and option:\n\tWater:\n\tSoda:\n\tCoffee:");
+        Console.Write("> ");
+        string button = Console.ReadLine();
+
+        switch (button)
+        {
+            case "Water":
+            case "water":
+                Console.WriteLine("Dispensing water.");
+                break;
+            case "Soda":
+            case "soda":
+                Console.WriteLine("Dispensing soda.");
+                break;
+            case "Coffee":
+            case "coffee":
+                Console.WriteLine("Dispensing coffee.");
+                break;
+            default:
+                Console.WriteLine("Wrong option.");
+                break;
+        }
+    }
+}
