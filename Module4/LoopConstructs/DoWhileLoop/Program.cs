a6e8be15c86f67fb7a4fbe59dff545b917ac6f39
pjsm <pjsm.lesi@gmail.com>
2025-03-28 20:21:25 +0000

    Created: DoWhile

---
+++ Module4/LoopConstructs/DoWhileLoop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int counter = 0;
+
+        do
+        {
+            Console.WriteLine(counter);
+            counter++;
+        } while (counter < 10);
+    }
+}
