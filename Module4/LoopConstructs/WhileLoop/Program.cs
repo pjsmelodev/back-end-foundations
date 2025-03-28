8781cf50b4ff50b4fea060c626ded72776d3b4aa
pjsm <pjsm.lesi@gmail.com>
2025-03-28 20:10:34 +0000

    Created: WhileLoop

---
+++ Module4/LoopConstructs/WhileLoop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int counter = 0;
+
+        while (counter < 10)
+        {
+            Console.WriteLine(counter);
+            counter++;
+        }
+    }
+}
