117b8acd070e311028fd8b9ec9a82436a6cd8479
pjsm <pjsm.lesi@gmail.com>
2025-03-28 17:11:53 +0000

    Created: CtrlStructs

---
+++ Module4/FundamentalsControlStructs/CtrlStructs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+    static void CheckTricketPrice(int age) {
+        if (age < 12)
+        {
+            Console.WriteLine("Half price ticket.");
+        }
+        else if (age >= 12 && age <= 65) {
+            Console.WriteLine("Full price ticket.");
+        }
+        else {
+            Console.WriteLine("Senior discount ticket.");
+        }
+    }
+
+    static void Main(string[] args) {
+        int age;
+
+        Console.Write("Enter age: ");
+        age = int.Parse(Console.ReadLine());
+
+        CheckTricketPrice(age);
+    }
+}
