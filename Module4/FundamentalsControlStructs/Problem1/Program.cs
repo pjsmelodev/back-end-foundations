b32250374d07c5127e0fbd53559dfe5689bd47be
pjsm <pjsm.lesi@gmail.com>
2025-03-28 18:48:29 +0000

    Created: Problem1

---
+++ Module4/FundamentalsControlStructs/Problem1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int age;
+        bool isPremium;
+
+        Console.Write("Enter age: ");
+        age = int.Parse(Console.ReadLine());
+
+        Console.Write("Are you a premium member? (yes or no) ");
+        isPremium = Console.ReadLine().ToLower() == "yes";
+
+        if (age < 18)
+        {
+            if (isPremium)
+            {
+                Console.WriteLine("Fee: $25.");
+            }
+            else {
+                Console.WriteLine("Fee: $15.");
+            }
+        }
+        else if (age >= 18 && age <= 60)
+        {
+            if (isPremium)
+            {
+                Console.WriteLine("Fee: $50.");
+            }
+            else {
+                Console.WriteLine("Fee: $30.");
+            }
+        }
+        else
+        {
+            if (isPremium)
+            {
+                Console.WriteLine("Fee: $35.");
+            }
+            else {
+                Console.WriteLine("Fee: $20.");
+            }
+        }
+    }
+}
