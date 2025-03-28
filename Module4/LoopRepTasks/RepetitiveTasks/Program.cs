0034b783a295e5f8ab0314c0a9491527e5bcd848
pjsm <pjsm.lesi@gmail.com>
2025-03-28 20:33:00 +0000

    Created: RepetitiveTasks

---
+++ Module4/LoopRepTasks/RepetitiveTasks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0) {
+                Console.WriteLine(numbers[i] + " is even.");
+            }
+            else
+            {
+                Console.WriteLine(numbers[i] + " is odd.");
+            }
+        }
+    }
+}
