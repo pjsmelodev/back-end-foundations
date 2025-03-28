afa5645ff0a2d2e1dfa627ffd9f996244bdbaffd
pjsm <pjsm.lesi@gmail.com>
2025-03-28 16:45:46 +0000

    Created: Practice2

---
+++ Module4/FundamentalsControlStructs/Practice2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Practice2 {
+    static void Main(string[] args) {
+        int score;
+
+        Console.Write("Enter student's score: ");
+        score = int.Parse(Console.ReadLine());
+
+        switch (score / 10)
+        {
+            case 10:
+            case 9:
+                Console.WriteLine("A");
+                break;
+            case 8:
+                Console.WriteLine("B");
+                break;
+            case 7:
+                Console.WriteLine("C");
+                break;
+            case 6:
+                Console.WriteLine("D");
+                break;
+            default:
+                Console.WriteLine("F");
+                break;
+        }
+        
+    }
+}
