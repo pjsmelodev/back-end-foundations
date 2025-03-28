efe2a47cb88bb6d0fd7dd3788631d4af4063530a
pjsm <pjsm.lesi@gmail.com>
2025-03-28 23:38:14 +0000

    Created: Example1

---
+++ Module4/IntegratedControlNLoops/Example1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int input;
+
+        do
+        {
+            Console.Write("Enter a number between 1 and 10: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input >= 1 && input <= 10)
+            {
+              Console.WriteLine("Valid input: " + input);
+              break;  
+            }
+            else {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        } while (true);
+    }
+}
