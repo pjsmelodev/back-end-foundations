dfbcb5bdd50384e4d7f55bf7e57d87b9055bf067
pjsm <pjsm.lesi@gmail.com>
2025-03-28 18:56:01 +0000

    Created: Problem2

---
+++ Module4/FundamentalsControlStructs/Problem2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        string accountType;
+
+        Console.WriteLine("Options:\n\tSavings\n\tChecking\n\tBusiness");
+        Console.Write("Enter account type: ");
+        accountType = Console.ReadLine().ToLower();
+
+        switch (accountType)
+        {
+            case "savings":
+                Console.WriteLine("Interest rate: 2%.");
+                break;
+            case "checking":
+                Console.WriteLine("Monthly fee: $10.");
+                break;
+            case "business":
+                Console.WriteLine("Interest rate: 1% and monthly fee: $20.");
+                break;
+            default:
+                Console.WriteLine("Error. Invalid option.");
+                break;
+        }
+        
+    }
+}
