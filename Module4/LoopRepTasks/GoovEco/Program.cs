e0be03a3de4382a94957ec833ec8d634e6a86d56
pjsm <pjsm.lesi@gmail.com>
2025-03-28 20:57:32 +0000

    Created: GoovEco

---
+++ Module4/LoopRepTasks/GoovEco/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program {
+    public static void Main(string[] args)
+    {
+       double[] prices = {10.00, 15.50, 8.25, 22.00} ;
+
+       for (int i = 0; i < prices.Length; i++)
+       {
+            prices[i] = prices[i] + 1.05;
+            Console.WriteLine("New price for product " + i + ": " + prices[i]);
+       }
+    }
+}
