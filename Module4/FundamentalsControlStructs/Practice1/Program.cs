6f4502a13b90813e37712600f0ed49b17e5875da
pjsm <pjsm.lesi@gmail.com>
2025-03-28 16:40:05 +0000

    Created: Practice1

---
+++ Module4/FundamentalsControlStructs/Practice1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Practice1 {
+    static void Main(string[] args) {
+        int grade;
+
+        Console.Write("Enter the student's grade: ");
+        grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 50)
+        {
+            Console.WriteLine("Passed.");
+        }
+        else {
+            Console.WriteLine("Flunked.");
+        }
+    }
+}
