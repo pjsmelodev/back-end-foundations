9d2c655033f81520ebe70242fd823610e96278fd
pjsm <pjsm.lesi@gmail.com>
2025-03-28 20:16:00 +0000

    Created: WhileLoop2

---
+++ Module4/LoopConstructs/WhileLoop2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        string input = "";
+
+        while (input != "exit")
+        {
+            Console.Write("Enter a command: ");
+            input = Console.ReadLine();
+        }
+    }
+}
