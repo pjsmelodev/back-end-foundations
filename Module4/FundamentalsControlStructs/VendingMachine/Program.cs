0e4daa43f611163c7d7504c7d06fd6c414c943d4
pjsm <pjsm.lesi@gmail.com>
2025-03-28 16:58:52 +0000

    Created: VendingMachine

---
+++ Module4/FundamentalsControlStructs/VendingMachine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class VendingMachine {
+    static void VendingMch(string choice) {
+        switch (choice) {
+            case "Water":
+            case "water":
+                Console.WriteLine("Dispensing water...");
+                break;
+            case "Soda":
+            case "soda":
+                Console.WriteLine("Dispensing soda...");
+                break;
+            case "Coffee":
+            case "coffee":
+                Console.WriteLine("Dispensing coffee...");
+                break;
+            default:
+                Console.WriteLine("Wrong option!");
+                break;
+        }
+    }
+
+    static void Main(string[] args) {
+        string choice;
+
+        Console.WriteLine("Options:\nWater\nSoda\nCoffee");
+        choice = Console.ReadLine();
+
+        VendingMch(choice);
+    }
+}
