22a7084403c7f90188c1788a009f4ec29b872357
pjsm <pjsm.lesi@gmail.com>
2025-03-29 18:53:44 +0000

    Created: Activity1

--- Module5/Methods/Activity1/Program.cs
+++ Module5/Methods/Activity1/Program.cs
@@ -7,6 +7,11 @@ public class Program
         return a + b;
     }
 
+    // Problem 1
+    public static decimal TrapezoidArea(decimal a, decimal b, decimal height) {
+        return (a + b) / 2 * height;
+    }
+
     public static void Main(string[] args)
     {
         // Example 1
@@ -19,5 +24,18 @@ public class Program
         int sum = AddNumbers(a, b);
 
         Console.WriteLine($"{a} + {b} = {sum}");
+
+        // Problem 1
+        Console.Write("Enter length of the fist parallel side: ");
+        decimal sideA = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Enter length of the second parallel side: ");
+        decimal sideB = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Enter height: ");
+        decimal height = decimal.Parse(Console.ReadLine());
+
+        decimal areaTrap = TrapezoidArea(sideA, sideB, height);
+        Console.WriteLine($"Parallel side A: {sideA}\nParallel side B: {sideB}\nHeight: {height}\nArea: {areaTrap}"); 
     }
 }
