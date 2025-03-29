dd9546b06b898d1a04b043a8e477288d1a922266
pjsm <pjsm.lesi@gmail.com>
2025-03-29 18:45:07 +0000

    Created: Activity1

---
+++ Module5/Methods/Activity1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    // Example 1
+    public static int AddNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void Main(string[] args)
+    {
+        // Example 1
+        Console.Write("Enter the first number: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int sum = AddNumbers(a, b);
+
+        Console.WriteLine($"{a} + {b} = {sum}");
+    }
+}
