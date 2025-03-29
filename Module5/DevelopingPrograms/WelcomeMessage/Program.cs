c6c2febb9e91889d9cbfc187557fd20c74e40508
pjsm <pjsm.lesi@gmail.com>
2025-03-29 22:33:55 +0000

    Created WelcomeMessage; Updated notes

---
+++ Module5/DevelopingPrograms/WelcomeMessage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void DisplayWelcomeMessage() {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    
+    public static void GreetUser(string user) {
+        Console.WriteLine($"Hello {user}!");
+    }
+
+    public static int CalculateSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void Main(string[] args)
+    {
+        DisplayWelcomeMessage();
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        GreetUser(name);
+
+        Console.Write("Enter your the first value: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter your the second value: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{a} + {b} = {CalculateSum(a, b)}");
+    }
+}
