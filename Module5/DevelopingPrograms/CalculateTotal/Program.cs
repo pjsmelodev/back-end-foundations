63bc5f149a62114744e488394ac08759a0c3cc40
pjsm <pjsm.lesi@gmail.com>
2025-03-29 21:25:17 +0000

    Created CalculateTotal

---
+++ Module5/DevelopingPrograms/CalculateTotal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static int CalculateTotalPrice(int[] itemPrices) {
+        int total = 0;
+
+        foreach (int price in itemPrices)
+        {
+            total += price;
+        }
+
+        return total;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] prices = {15, 16, 18, 19};
+        int total = CalculateTotalPrice(prices);
+
+        Console.WriteLine("Total: " + total);
+    }
+}
