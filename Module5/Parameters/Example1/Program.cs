1b1411c840038ba853caff6d3fc4badbe47d1137
pjsm <pjsm.lesi@gmail.com>
2025-03-29 20:26:49 +0000

    Created Example1; Updated notes

---
+++ Module5/Parameters/Example1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+    public static void GreetUser(string userName) {
+        Console.WriteLine("Hello, " + userName + "!");
+    }
+
+    public static void Main(string[] args)
+    {
+        GreetUser("Paulo");
+    }
+}
