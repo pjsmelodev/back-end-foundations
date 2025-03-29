818daf67f613027352dd9c3981070d08edbc0e75
pjsm <pjsm.lesi@gmail.com>
2025-03-29 17:06:45 +0000

    Created: Func1

---
+++ Module5/IntroFunctions/Func1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static int calculateTotal(int[] items) {
+        int sum = 0;
+
+        foreach (var item in items)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] prices = {10, 3, 15, 19, 25};
+        int sum = calculateTotal(prices);
+
+        Console.WriteLine("The sum is: " + sum);
+    }
+}
