41e8fe8a6a236bede8da2599212ef0a49a801f73
pjsm <pjsm.lesi@gmail.com>
2025-03-29 22:11:00 +0000

    Created Discount

---
+++ Module5/DevelopingPrograms/Discount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static double CalculateDiscount(double price, double discountRate) {
+        return price - (price * discountRate);
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter price: ");
+        double price = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter discount rate: ");
+        double discountRate = (double.Parse(Console.ReadLine()) / 100);
+
+        Console.WriteLine("Discount: " + CalculateDiscount(price, discountRate));
+    }
+}
