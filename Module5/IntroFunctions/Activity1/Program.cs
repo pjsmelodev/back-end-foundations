1ff2599d07572dccd6c88159675c07f2857e4147
pjsm <pjsm.lesi@gmail.com>
2025-03-29 17:36:39 +0000

    Updated: Activity1

--- Module5/IntroFunctions/Activity1/Program.cs
+++ Module5/IntroFunctions/Activity1/Program.cs
@@ -2,15 +2,20 @@
 
 public class Program
 {
-    // Example 1
     public static double CalculateRectangleArea(double length, double width) {
-        double area = length * width;
-        return area;
+        return length * width;
+    }
+
+    public static double CalculateTriangleArea(double baseTri, double height) {
+        return baseTri * height / 2;
+    }
+
+    public static double CircleArea(double radius) {
+        return Math.PI * Math.Pow(radius, 2);
     }
 
     public static void Main(string[] args)
     {
-        // Example 1
         double length, width;
 
         Console.Write("Enter length of the rectangle: ");
@@ -19,8 +24,26 @@ public class Program
         Console.Write("Enter width of the rectangle: ");
         width = double.Parse(Console.ReadLine());
 
-        double area = CalculateRectangleArea(length, width);
+        double areaRec = CalculateRectangleArea(length, width);
+        Console.WriteLine($"The area of the rectangle with length: {length} and width: {width} is: {areaRec}");
 
-        Console.WriteLine($"The area of the rectangle with length: {length} and width: {width} is: {area}");
+        double baseTri, height;
+
+        Console.Write("Enter the base of the triangle: ");
+        baseTri = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the height of the triangle: ");
+        height = double.Parse(Console.ReadLine());
+
+        double areaTri = CalculateTriangleArea(baseTri, height);
+        Console.WriteLine($"The area of the triangle is: {areaTri}");
+
+        double radius;
+
+        Console.Write("Enter radius of the circle: ");
+        radius = double.Parse(Console.ReadLine());
+
+        double areaCircle = CircleArea(radius);
+        Console.WriteLine($"The area of the circle with radius {radius} is: {areaCircle}");
     }
 }
