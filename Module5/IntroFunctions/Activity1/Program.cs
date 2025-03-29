87ccbdcf618dab72e938ff1ac2dc5fe487b1f1b0
pjsm <pjsm.lesi@gmail.com>
2025-03-29 17:20:27 +0000

    Created: Activity1

---
+++ Module5/IntroFunctions/Activity1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    // Example 1
+    public static double CalculateRectangleArea(double length, double width) {
+        double area = length * width;
+        return area;
+    }
+
+    public static void Main(string[] args)
+    {
+        // Example 1
+        double length, width;
+
+        Console.Write("Enter length of the rectangle: ");
+        length = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter width of the rectangle: ");
+        width = double.Parse(Console.ReadLine());
+
+        double area = CalculateRectangleArea(length, width);
+
+        Console.WriteLine($"The area of the rectangle with length: {length} and width: {width} is: {area}");
+    }
+}
