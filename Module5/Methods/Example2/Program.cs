a8daf116f94d2597cd8f49997055c9391a26a860
pjsm <pjsm.lesi@gmail.com>
2025-03-29 18:35:56 +0000

    Created: Example2

---
+++ Module5/Methods/Example2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static decimal CalculateTotal(List<decimal> prices) {
+        decimal total = 0;
+
+        for (int i = 0; i < prices.Count; i++)
+        {
+            total += prices[i];
+        }
+
+        return total;
+    }
+
+    public static void Main(string[] args)
+    {
+        List<decimal> values = new List<decimal> {15, 12, 13, 19};
+
+        decimal total = CalculateTotal(values);
+
+        Console.WriteLine("The total is: " + total);
+    }
+}
