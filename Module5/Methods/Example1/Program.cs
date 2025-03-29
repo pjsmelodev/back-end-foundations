8aa9db7e702def196f9cf4c3092bc6cd5ad3d63f
pjsm <pjsm.lesi@gmail.com>
2025-03-29 18:00:01 +0000

    Updated: notes; Created: Example1

---
+++ Module5/Methods/Example1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static double CalculateDiscount(double originalPrice, double discountRate) {
+        double origPrice = originalPrice;
+        double discRate = discountRate;
+
+        return origPrice * discRate;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter price: ");
+        double price = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter discount rate: (double value) ");
+        double discountRate = double.Parse(Console.ReadLine());
+
+        double discount = CalculateDiscount(price, discountRate);
+        Console.WriteLine("Discount is: " + discount);
+    }
+}
